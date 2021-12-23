a5e802097cf8dd3168da8b24e20ad3dc68e38b18
kobas <gorkowski.piotr1@gmail.com>
2021-12-23 13:06:09 +0100

    final commit

---
+++ cmake-build-debug/CMakeFiles/friday_the_13th_game.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/friday_the_13th_game.dir/main.cpp.obj"
+  "friday_the_13th_game.exe"
+  "friday_the_13th_game.exe.manifest"
+  "friday_the_13th_game.pdb"
+  "libfriday_the_13th_game.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/friday_the_13th_game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
